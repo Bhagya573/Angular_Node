ee4faacd990e4fe4c06bf8619606afd64cfe3704
bhagya patil <bhagyshree.p@mail.waldenu.edu>
2024-12-26 13:15:05 +0530

    Delete

--- Backend-Quote/middlewares/actionLogger.js
+++
@@ -1,8 +0,0 @@
-  // actionLogger.jsy
-const logger = require("../logger");
-const logAction = (action, additionalInfo = '') => {
-  const logMessage = `${action}${additionalInfo ? ' - ' + additionalInfo : ''}`;
-  logger.info(logMessage); 
-};
-
-module.exports = logAction;
